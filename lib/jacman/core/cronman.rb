012431dfd582b7fd071faaada3b88b1492205a7d
Michel Demazure <michel@demazure.com>
2014-12-20 15:41:35 +0100

    2.2.1

---
+++ lib/jacman/core/cronman.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+# File: cronman
+# Created: 16/06/14
+#
+# (c) Michel Demazure <michel@demazure.com>
+
+JacintheManagement::Core::Command.cron_run(ARGV.first)
---
+++ lib/jacman/core/jacdev.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+# File: jacdev
+# Created: 16/06/14
+#
+# (c) Michel Demazure <michel@demazure.com>
+
+JacintheManagement::Core::Cli.developer.run(ARGV)
